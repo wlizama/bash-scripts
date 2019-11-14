61105e28472fd2354e17630b19f226c28d15477f
Wilder Lizama Garcia <wilderlizama@gmail.com>
2019-11-14 16:15:26 -0500

    desktop change ubuntu

--- ubuntu-desktop-bg.sh
+++ ubuntu-desktop-bg.sh
@@ -10,8 +10,23 @@
 # simple url decode
 urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
 
+# solución ejecucion desde crontab
+# * * * * * ~/WildPersonal/develop/bash-scripts/ubuntu-desktop-bg.sh
+# https://askubuntu.com/questions/140305/cron-not-able-to-succesfully-change-background#answer-198508
+user=$(whoami)
+
+fl=$(find /proc -maxdepth 2 -user $user -name environ -print -quit)
+while [ -z $(grep -z DBUS_SESSION_BUS_ADDRESS "$fl" | cut -d= -f2- | tr -d '\000' ) ]
+do
+  fl=$(find /proc -maxdepth 2 -user $user -name environ -newer "$fl" -print -quit)
+done
+
+export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS "$fl" | cut -d= -f2-)
+
+
+
 # Folder contenedor de imágenes
-PATH_WALLPAPERS=/home/wilder/Imágenes/wallpapers
+PATH_WALLPAPERS=~/Imágenes/wallpapers
 
 img_names=($(ls "$PATH_WALLPAPERS"/))
 
@@ -21,6 +36,7 @@ current_bg_dec=$(urldecode "$current_bg")
 idx_next=0
 for img_idx in "${!img_names[@]}"; do
     full_path=$PATH_WALLPAPERS/${img_names[img_idx]}
+    # verifica la imagen actual para aumentar indice de imagen siguiente
     if [[ $current_bg_dec == *$full_path* ]]; then
         idx_next=$((img_idx + 1))
     fi
@@ -31,12 +47,16 @@ if (( ${#img_names[idx_next]} < 1 )); then
     idx_next=0
 fi
 
+path_img_next="${PATH_WALLPAPERS}/${img_names[idx_next]}"
+
 # verifica si archivo existe
-if [[ ! -f "$full_path" ]]; then
+if [[ ! -f "$path_img_next" ]]; then
     idx_next=0
 fi
 
 # set background
-gsettings set org.gnome.desktop.background picture-uri "file://${PATH_WALLPAPERS}/${img_names[idx_next]}"
+DISPLAY=:0 GSETTINGS_BACKEND=dconf gsettings set org.gnome.desktop.background picture-uri file://$path_img_next
 
-echo ${img_names[idx_next]}
+printf "Nuevo: $path_img_next \n  Ant: $current_bg_dec\n\n"
+
+exit 0
